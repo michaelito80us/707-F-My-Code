0670525630f5ff567d3b9f3f6bbb1f2108a1969e
Sergey Filippov <sergey.filippov.89@inbox.ru>
2021-12-02 20:15:40 +0800

    added show page with delete functionality

--- pages/stories/index.js
+++ pages/stories/index.js
@@ -12,23 +12,10 @@ Page({
 
   },
 
-  /**
-   * Lifecycle function--Called when page load
-   */
   onLoad: function (options) {
 
   },
 
-  /**
-   * Lifecycle function--Called when page is initially rendered
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page show
-   */
   onShow: function () {
     const page = this
     wx.request({
@@ -39,42 +26,10 @@ Page({
         page.setData(res.data)
       }
     })
-
-
   },
-
-  /**
-   * Lifecycle function--Called when page hide
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * Lifecycle function--Called when page unload
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * Page event handler function--Called when user drop down
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * Called when page reach bottom
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * Called when user click on the top right corner to share
-   */
-  onShareAppMessage: function () {
-
+  navigateToStoryShow(e){
+    wx.navigateTo({
+      url: `/pages/stories/show?id=${e.mark.id}`,
+    })
   }
 })
---
+++ pages/stories/show.js
@@ -0,0 +1,62 @@
+// pages/stories/show.js
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  onLoad: function (options) {
+    console.log(options)
+    this.setData({
+      options
+    })
+  },
+
+
+  onShow: function () {
+    const page = this
+    wx.request({
+      url: `${getApp().globalData.baseUrl}/stories/${page.data.options.id}`,
+      success(res) {
+        page.setData(res.data)
+      }
+    })
+  },
+
+  deleteStory() {
+    const page = this
+    wx.showModal({
+      title: "Delete story?",
+      confirmText: "Yes",
+      cancelText: "No",
+      success(res) {
+        if(res.confirm){
+          
+          wx.request({
+            url: `${getApp().globalData.baseUrl}/stories/${page.data.options.id}`,
+            method: 'DELETE',
+            success(res) {
+              wx.showToast({
+                icon: 'none',
+                title: `${res.data.message}`,
+                success() {
+                  setTimeout(page.switchToIndex, 500)
+                }
+              })
+  
+            }
+          })
+        }
+      }
+    })
+  },
+  switchToIndex() {
+    wx.switchTab({
+      url: '/pages/stories/index',
+    })
+  }
+
+})
